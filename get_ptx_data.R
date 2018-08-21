f08dd2eac7b10926ed6441d35a58399dccf9c36a
Rafe Liu <snexuz@gmail.com>
2018-08-21 11:04:31 +0800

    ptx_v2

--- get_ptx_data.R
+++ get_ptx_data.R
@@ -1,4 +1,3 @@
-library(dplyr)
 library(httr)
 
 get_ptx_data <- function(app_id, app_key, url){
@@ -7,7 +6,7 @@ get_ptx_data <- function(app_id, app_key, url){
   Sys.setlocale("LC_TIME", "en_US.UTF8")
   
   # "Tue, 21 Aug 2018 01:18:42 GMT"
-  xdate <- as.POSIXlt(Sys.time(), tz = "GMT") %>% format("%a, %d %b %Y %H:%M:%S GMT")
+  xdate <- format(as.POSIXlt(Sys.time(), tz = "GMT"), "%a, %d %b %Y %H:%M:%S GMT")
   sig <- httr::hmac_sha1(app_key, paste("x-date:", xdate)) 
   
   # hmac username="APP ID", algorithm="hmac-sha1", headers="x-date", 
@@ -23,11 +22,13 @@ get_ptx_data <- function(app_id, app_key, url){
     'Authorization'= authorization,
     'x-date'= as.character(xdate))
   
-  dat <- GET(url, config = config(ssl_verifypeer = 0L), 
+  dat <- GET(url, 
+             config = config(ssl_verifypeer = 0L), 
              add_headers(.headers = auth_header))
   
-  
   print(http_status(dat)$message)
+  
+  # Set back to origin locale
   Sys.setlocale('LC_TIME', loc)
   
   # return(dat)
@@ -35,14 +36,3 @@ get_ptx_data <- function(app_id, app_key, url){
 }
 
 
-app_id <- ''
-app_key <- ''
-url <- 'https://ptx.transportdata.tw/MOTC/v2/Bus/Stop/City/YilanCounty?$top=30&$format=xml'
-
-x <- get_ptx_data(app_id, app_key, url)
-
-library(XML)
-dat <- xmlParse(x, encoding = 'utf-8') # 以 xmlParse 解析 XML 檔案
-xmlfiles <- xmlRoot(dat) # 將 root 設定到 content 層級（一個偷吃步的做法）
-y <- xmlToDataFrame(xmlfiles, stringsAsFactors = FALSE) # 轉換成 dataframe
-head(y)
